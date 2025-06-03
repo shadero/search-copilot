5fdade3ade76d61f3d8ba8604d349dff67f6e2fc
shadero <shadero.vrc@gmail.com>
2025-06-04 00:08:22 +0900

    Refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,8 @@ import ReactDOM from 'react-dom/client'
 import AppRoutes from './react-router/AppRoutes.tsx'
 // import './index.css'
 
-function App() {
-  return (
-    <div>
-      <h1>Welcome to the React App</h1>
-      <p>This is a simple application using React and React Router.</p>
-    </div>
-  )
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppRoutes />
   </React.StrictMode>,
 )
-
-export default App;
