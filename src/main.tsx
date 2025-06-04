00516bbc80ef365f084a9a16863bfbd21f7078d4
shadero <shadero.vrc@gmail.com>
2025-06-05 04:26:50 +0900

    Import Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from './react-router/AppRoutes.tsx'
 import { NuqsAdapter } from 'nuqs/adapters/react-router/v7'
-// import './index.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
