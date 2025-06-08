8a380723a5a12e8c80839e7b5fe295f6ec1a0f0c
shadero <shadero.vrc@gmail.com>
2025-06-08 19:37:01 +0900

    Change Top Page

--- src/TopPage.tsx
+++ src/TopPage.tsx
@@ -5,11 +5,11 @@ function MainContent() {
 		<div>
 			<div>
 				<h1 className="text-4xl font-bold mb-4">検索アプリへようこそ</h1>
-				<p className="text-lg mb-6">検索バーを使ってノート、ハッシュタグ、Google検索結果を探せます。</p>
+				<p className="text-lg mb-6">ノートやハッシュタグ、Google検索結果を探せます。</p>
 				<div className="flex gap-4 flex-wrap">
 
-					<a href="/search" className="btn btn-primary">検索ページ</a>
-					<a href="/suggestKeywords" className="btn btn-primary">キーワードサジェスト</a>
+					<a href="/search" className="btn btn-primary">記事検索</a>
+					<a href="/suggestKeywords" className="btn btn-primary">サジェスト</a>
 				</div>
 			</div>
 		</div>
