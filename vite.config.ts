9a0c930208fd47db13b87fb09e3844df8b296773
shadero <shadero.vrc@gmail.com>
2025-06-29 21:09:05 +0900

    Add env BASE_PATH

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/search-copilot',
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-})
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		base: env.VITE_BASE_PATH || '/',
+		plugins: [
+			tailwindcss(),
+			react()
+		],
+	}
+});
